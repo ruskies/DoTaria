19ba40edac3b403704ca523733ee761f78cb93fc
webmilio <therealwebmilio@gmail.com>
2019-07-20 16:18:06 -0400

    Attributes are no longer saved, but calculated.

--- Players/Attributes/DoTariaPlayer.Attributes.Saving.cs
+++ Players/Attributes/DoTariaPlayer.Attributes.Saving.cs
@@ -5,16 +5,5 @@ namespace DoTaria.Players
 {
     public sealed partial class DoTariaPlayer : ModPlayer
     {
-        private void SaveAttributes(TagCompound tag)
-        {
-            tag.Add(nameof(Strength), Strength);
-            tag.Add(nameof(Agility), Agility);
-            tag.Add(nameof(Intelligence), Intelligence);
-        }
-
-        private void LoadAttributes(TagCompound tag)
-        {
-
-        }
     }
 }
