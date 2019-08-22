e411c3841f853a106b2c660455f941f6df66fe78
webmilio <therealwebmilio@gmail.com>
2019-08-22 19:34:55 -0400

    Added on npc kill packet for servers.

--- NPCs/DoTariaGlobalNPC.cs
+++ NPCs/DoTariaGlobalNPC.cs
@@ -1,5 +1,8 @@
-﻿using DoTaria.Players;
+﻿using DoTaria.Helpers;
+using DoTaria.Network;
+using DoTaria.Players;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace DoTaria.NPCs
@@ -8,12 +11,18 @@ namespace DoTaria.NPCs
     {
         public override void NPCLoot(NPC npc)
         {
+            if (npc.lastInteraction == 255) return; // Killed by a non-player.
+
+            if (Main.netMode == NetmodeID.Server)
+                NetworkPacketManager.Instance.PlayerKilledNPC.SendPacketToAllClients(255, npc.lastInteraction, NPCsHelper.GetNPCIdFromNPC(npc));
+            else if (npc.lastInteraction != Main.myPlayer) return; // Killed by someone else.
+
             DoTariaPlayer dotariaPlayer = DoTariaPlayer.Get(Main.LocalPlayer);
 
             if (dotariaPlayer == null)
                 return;
 
-            if (npc.lastInteraction == Main.LocalPlayer.whoAmI)
+            if (npc.lastInteraction == Main.myPlayer)
                 dotariaPlayer.OnKilledNPC(npc);
         }
     }
