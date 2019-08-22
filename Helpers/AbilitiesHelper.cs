92db81096f97fa007d8dff23b4610ba9fa1a00c3
webmilio <therealwebmilio@gmail.com>
2019-08-22 19:32:38 -0400

    Added helpers for NPCs and Abilities.

---
+++ Helpers/AbilitiesHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using DoTaria.Abilities;
+
+namespace DoTaria.Helpers
+{
+    public static class AbilitiesHelper
+    {
+        public static string SerializeAbilities(params PlayerAbility[] abilities)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < abilities.Length; i++)
+            {
+                sb.Append(abilities[i].Ability.UnlocalizedName).Append(':').Append(abilities[i].Level).Append(':').Append(abilities[i].Cooldown);
+
+                if (i + 1 < abilities.Length)
+                    sb.Append(",");
+            }
+
+            return sb.ToString();
+        }
+
+        public static PlayerAbility[] DeserializeAbilities(string serializedAbilities)
+        {
+            string[] splitAbilities = serializedAbilities.Split(',');
+            PlayerAbility[] abilities = new PlayerAbility[splitAbilities.Length];
+
+            for (int i = 0; i < splitAbilities.Length; i++)
+            {
+                string[] unparsedAbility = splitAbilities[i].Split(':');
+                abilities[i] = new PlayerAbility(AbilityDefinitionManager.Instance[unparsedAbility[0]], int.Parse(unparsedAbility[1]), int.Parse(unparsedAbility[2]));
+            }
+
+            return abilities;
+        }
+    }
+}
---
+++ Helpers/NPCsHelper.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+
+namespace DoTaria.Helpers
+{
+    public static class NPCsHelper
+    {
+        public static int GetNPCIdFromNPC(NPC npc)
+        {
+            for (int i = 0; i < Main.npc.Length; i++)
+                if (Main.npc[i] == npc)
+                    return i;
+
+            return 0;
+        }
+
+        public static NPC GetNPCFromNetId(int npcNetId)
+        {
+            for (int i = 0; i < Main.npc.Length; i++)
+                if (Main.npc[i].netID == npcNetId)
+                    return Main.npc[i];
+
+            return null;
+        }
+    }
+}
